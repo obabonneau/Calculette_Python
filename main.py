2a5142fbbd178b5185a91d82bc61573a8b352ae1
Olivier BABONNEAU <obabonneau@gmail.com>
2024-07-20 14:34:44 +0200

    Modification de l'ordre des fonctions / Ajout de commentaires

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
+# Définition de la fonction de bienvenue
 def print_welcome_message():
     print("Bienvenue sur la mini-calculatrice !")
-    
-def input_two_number():
-    num1 = float(input("Entrez le premier nombre : "))
-    num2 = float(input("Entrez le deuxième nombre : "))
-    return num1, num2
 
+# Définition de la fonction du choix de l'opération
 def print_menu_and_get_choice():
     print("=== MENU ===")
     print("1. Addition")
@@ -14,28 +11,12 @@ def print_menu_and_get_choice():
     print("4. Division")
 
     user_choice = input("Entrez votre choix (1-4) : ")
-
     while user_choice not in ["1", "2", "3", "4"]:
-
         user_choice = input("Choix invalide. Entrez votre choix (1-4) : ")
 
     return user_choice
 
-def sum(a, b):
-    return a + b
-
-def substraction(a, b):
-    return a - b
-
-def multiplication(a, b):
-    return a * b
-
-def division(a, b):
-    if b != 0:
-        return a / b
-    else:
-        print("Erreur : division par zéro")
-
+# Définition de la fonction de calcul
 def run_calculation(user_choice):
     num1, num2 = input_two_number()
     match user_choice:
@@ -51,6 +32,32 @@ def run_calculation(user_choice):
             print("Choix invalide.")
     return result
 
+# Définition de la fonction de saisie des nombres
+def input_two_number():
+    num1 = float(input("Entrez le premier nombre : "))
+    num2 = float(input("Entrez le deuxième nombre : "))
+    return num1, num2
+
+# Définition de la fonction addition
+def sum(a, b):
+    return a + b
+
+# Définition de la fonction soustraction
+def substraction(a, b):
+    return a - b
+
+# Définition de la fonction multiplication
+def multiplication(a, b):
+    return a * b
+
+# Définition de la fonction division
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        print("Erreur : division par zéro")
+
+# Programme autolancé
 if __name__ == '__main__':
     print_welcome_message()
     user_choice = print_menu_and_get_choice()
